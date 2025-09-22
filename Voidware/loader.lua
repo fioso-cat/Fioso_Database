dd1ae500bddf2f14437752993461fa18f2342dd1
fioso-cat <fiosocat@gmail.com>
2025-09-22 11:34:28 +0700

    Create loader.lua

---
+++ Voidware/loader.lua
@@ -0,0 +1,23 @@
+local Fioso = {}
+local CONFIG = {
+    repo = "https://raw.githubusercontent.com/fioso-cat/Fioso_Database/refs/heads/main/Voidware/99NightForest/",
+    tab = {
+        Characters = "Characters.json",
+        Items = "Items.json",
+        Trees = "Trees.json",
+    }
+}
+
+for _, Fetch in pairs(CONFIG.tab) do
+    local success, response = pcall(function()
+        return game:HttpGet(CONFIG.repo .. Fetch)
+    end)
+    if success then
+        local data = game:GetService("HttpService"):JSONDecode(response)
+        table.insert(Fioso, data)
+    else
+        warn("Failed to fetch " .. Fetch .. ": " .. tostring(response))
+    end
+end
+
+return Fioso
